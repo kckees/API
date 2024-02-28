8ff57606cebfdac1c84e638f1acc9bdb3689aec1
kckees <160584419+kckees@users.noreply.github.com>
2024-02-28 12:58:41 +0100

    first commit

---
+++ public_html/index.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function(Request $request, Response $response) {
+    $response->getBody()->write("hello world");
+    return $response;
+
+});
+
+$app->run();
